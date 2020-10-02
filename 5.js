c9d84c7f8be86b354df1bdf36fd1e212305a74b2
tranglu <lu.t.t.trang@gmail.com>
2020-10-02 14:36:46 +0700

    lựa chọn hiệu ứng

--- 5.js
+++ 5.js
@@ -1,6 +1,7 @@
 //xử lý đóng gói code && toán tử 3 ngôi
 document.addEventListener("DOMContentLoaded",function(){
 			window.ID=0;
+			window.luachon=1;
 		var luachon=document.querySelectorAll('.btn');
 		for (var i = 0; i < luachon.length; i++) {
 		luachon[i].onclick=function(){
@@ -9,6 +10,7 @@ document.addEventListener("DOMContentLoaded",function(){
 			}
 			this.classList.toggle('active');
 			window.luachon=this.getAttribute('data-id');
+		}
 			var nutPhai=document.querySelector('.chuyenslide b.phai'),
 			nutTrai=document.querySelector('.chuyenslide b.trai');
 		var slides=document.querySelectorAll('.motslide');
@@ -23,15 +25,14 @@ function xacDinh2SlideVaChuyenDong(nut,i){
 	window.ID=(nut=='nutPhai')?((window.ID==(slides.length-1))?0:window.ID+1):((window.ID==0)?slides.length-1:window.ID-1);
 	var slideTiepTheo=slides[window.ID];
 	// kiểm tra kết thúc chuyển động
-	var xuLyHienTaiKetThucChuyenDong=function(){		
-			
-			this.classList.remove((nut=='nutPhai')?'bienMatKhiAnNext'.i:'bienMatKhiAnPre'.i);
+	var xuLyHienTaiKetThucChuyenDong=function(){				
+			this.classList.remove((nut=='nutPhai')?'bienMatKhiAnNext'+i:'bienMatKhiAnPre'+i);
 			this.classList.remove('xuathien');
 			trangthai2chuyendong++;
 			trangthai=(trangthai2chuyendong==2)?'dungyen':trangthai;
 		}
 	var xuLyTiepTheoKetThucChuyenDong=function(){
-			this.classList.remove((nut=='nutPhai')?'hienRaKhiAnNext'.i:'hienRaKhiAnPre'.i);
+			this.classList.remove((nut=='nutPhai')?'hienRaKhiAnNext'+i:'hienRaKhiAnPre'+i);
 			this.classList.add('xuathien');
 			trangthai2chuyendong++;
 			trangthai=(trangthai2chuyendong==2)?"dungyen":trangthai;
@@ -39,22 +40,22 @@ function xacDinh2SlideVaChuyenDong(nut,i){
 		slideHienTai.addEventListener('webkitAnimationEnd',xuLyHienTaiKetThucChuyenDong);
 		slideTiepTheo.addEventListener('webkitAnimationEnd',xuLyTiepTheoKetThucChuyenDong);
 		// toán tử 3 ngôi chổ này
-		slideHienTai.classList.add((nut=='nutPhai')?'bienMatKhiAnNext'.i:'bienMatKhiAnPre'.i);
-		slideTiepTheo.classList.add((nut=='nutPhai')?'hienRaKhiAnNext'.i:'hienRaKhiAnPre'.i);
+		slideHienTai.classList.add((nut=='nutPhai')?'bienMatKhiAnNext'+i:'bienMatKhiAnPre'+i);
+		slideTiepTheo.classList.add((nut=='nutPhai')?'hienRaKhiAnNext'+i:'hienRaKhiAnPre'+i);
 }
 	// hàm chuyển slide phải
 	var chuyenSlidePhai=function(){
-	xacDinh2SlideVaChuyenDong('nutPhai',4);
+	xacDinh2SlideVaChuyenDong('nutPhai',window.luachon);
 	};
 	 // hàm chuyển slide trái
 	var chuyenSlideTrai=function(){
-	xacDinh2SlideVaChuyenDong('nutTrai',4);
+	xacDinh2SlideVaChuyenDong('nutTrai',window.luachon);
 	};
 	 //hành động
 	nutPhai.addEventListener('click',chuyenSlidePhai);
 	nutTrai.addEventListener('click',chuyenSlideTrai);
 		}
-	}
+	
 		
 	},false);
 	
